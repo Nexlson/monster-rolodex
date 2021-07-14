d52ecfe33ecb2a28b10eded15e9aab2d6120bf2a
Nexlson <nelsonchoon112@gmail.com>
2021-07-14 21:08:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import { CardList } from './components/card_list/card-list.component';
+import {SearchBox} from './components/search_box/search-box.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Class implementation
+class App extends Component{
+  constructor(){
+
+    super();
+
+    this.state = {
+      monsters: [],
+      searchKeyWord: ''
+    }
+
+    // Pass this context to function
+    // this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users}))
+  }
+
+  //  Or use Es6 Syntax (Arrow function)
+  handleChange = (e) => {
+    this.setState({searchKeyWord: e.target.value})
+  }
+
+  render(){
+    const { monsters, searchKeyWord } = this.state;
+    const filtered = monsters.filter(monster => monster.name.toLowerCase().includes(searchKeyWord.toLowerCase()))
+
+
+    return (
+      <div className="App">
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder="monster name" handleChange={this.handleChange}/>
+        <CardList monsters={filtered}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
